4571b7de051b2c01ee02c6b9e7fe266a93ee28b7
Vladymyr <vladymyr.a13@gmail.com>
2020-10-23 18:28:50 +0300

    Сайт полностью адаптирован

--- js/main.js
+++ js/main.js
@@ -13,4 +13,22 @@ $(function () {
         $(this).parent().addClass('questions__item--active')
     })
 
+    $('#fullpage').fullpage({
+        autoScrolling: true,
+        scrollHorizontally: true,
+        sectionSelector: '.page-section',
+        scrollOverflow: true,
+        menu: '#header__nav',
+        anchors: ['top', 'products', 'benefits', 'specification', 'questions', 'contacts'],
+    });
+
+    $('.menu__btn').on('click', function () {
+        $('.menu__btn').toggleClass('menu__btn--active')
+        $('.menu__list').toggleClass('menu__list--active')
+    })
+    $('.menu__list-link').on('click', function () {
+        $('.menu__btn').removeClass('menu__btn--active')
+        $('.menu__list').removeClass('menu__list--active')
+    })
+
 })
